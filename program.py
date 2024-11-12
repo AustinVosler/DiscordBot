e53f4856517b37ee9d3a88ff393c40160aeea9cc
Austin Vosler <austinpvosler@gmail.com>
2024-11-11 20:06:52 -0500

    Sample DB setup

--- program.py
+++ program.py
@@ -1,6 +1,9 @@
 import discord
 import os # default module
 from dotenv import load_dotenv
+import sqlite3
+
+from pkg_resources import safe_name
 
 load_dotenv() # load all the variables from the env file
 bot = discord.Bot()
@@ -13,4 +16,22 @@ async def on_ready():
 async def hello(ctx: discord.ApplicationContext):
     await ctx.respond("Hey!")
 
-bot.run(str(os.getenv('token'))) # run the bot with the token
+# bot.run(str(os.getenv('token'))) # run the bot with the token
+
+con = sqlite3.connect("messages.db")
+
+cur = con.cursor()
+
+def print_all_messages(table_name):
+    for row in cur.execute("SELECT * FROM {}".format(table_name)):
+        print(row)
+    
+
+print("All data entries:")
+print_all_messages("Sample_Messages")
+
+print("\nSorted by score:")
+for row in cur.execute("SELECT name, score FROM Sample_Messages ORDER BY score DESC"):
+    print(row)
+
+# print(f"")
