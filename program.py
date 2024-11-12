2449fb0b65c1c59a9becbcc4e3fa3cc0d8fc22f2
Austin Vosler <austinpvosler@gmail.com>
2024-11-11 22:21:40 -0500

    Puts messages into db when reaction placed

--- program.py
+++ program.py
@@ -1,37 +1,68 @@
+from datetime import datetime
 import discord
-import os # default module
+import os
 from dotenv import load_dotenv
 import sqlite3
-
-from pkg_resources import safe_name
+import random
 
 load_dotenv() # load all the variables from the env file
-bot = discord.Bot()
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = discord.Bot(intents=intents)
+
+def fix_name(name):
+    new_name = ""
+    for char in name:
+        if (char == '\''):
+            continue
+        if (char.isspace()):
+            new_name += '_'
+            continue
+        new_name += char
+            
+    return new_name
 
 @bot.event
 async def on_ready():
     print(f"{bot.user} is ready and online!")
 
+@bot.event
+async def on_raw_reaction_add(payload):
+    if (payload.emoji.name != "🔍"):
+        return
+    
+    message = await bot.get_channel(payload.channel_id).fetch_message(payload.message_id)
+
+    guild_name = fix_name(bot.get_channel(payload.channel_id).guild.name)
+    user = message.author
+
+    print("id {} name {}".format(user.id, user.name))
+    print("Message reacted to: \"{}\" with {} in channel {} in {} at {}\n"
+            .format(message.content, payload.emoji.name, 
+                    bot.get_channel(payload.channel_id), 
+                    bot.get_channel(payload.channel_id).guild.name,
+                    datetime.now()))
+
+    
+    res = cur.execute("SELECT name FROM sqlite_master WHERE name LIKE '{}'".format(guild_name))
+    if (res.fetchone() == None):
+        cur.execute("CREATE TABLE {}(id, message_id, message, score)".format(guild_name))
+
+    res = cur.execute("SELECT message_id FROM {} WHERE message_id LIKE '{}'".format(guild_name, message.id))
+    if (res.fetchone() == None):
+        cur.execute("INSERT INTO {} VALUES ({},{},'{}',{})".format(guild_name, user.id, message.id, message.content, random.randint(1, 100)))
+        con.commit()
+
 @bot.slash_command(name="hello", description="Say hello to the bot")
 async def hello(ctx: discord.ApplicationContext):
     await ctx.respond("Hey!")
 
-# bot.run(str(os.getenv('token'))) # run the bot with the token
-
 con = sqlite3.connect("messages.db")
-
 cur = con.cursor()
+
+bot.run((os.getenv('token'))) # run the bot with the token
 
 def print_all_messages(table_name):
     for row in cur.execute("SELECT * FROM {}".format(table_name)):
         print(row)
-    
-
-print("All data entries:")
-print_all_messages("Sample_Messages")
-
-print("\nSorted by score:")
-for row in cur.execute("SELECT name, score FROM Sample_Messages ORDER BY score DESC"):
-    print(row)
-
-# print(f"")
