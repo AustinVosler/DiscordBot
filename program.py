f8cb546d05aea52c9d819add981add52419c07b1
Austin Vosler <austinpvosler@gmail.com>
2024-11-12 02:17:59 -0500

    Paginator Working, list out sorted messages /list-messages

--- program.py
+++ program.py
@@ -4,8 +4,12 @@ import os
 from dotenv import load_dotenv
 import sqlite3
 import random
+from discord.ext.pages import Paginator, Page
+from discord.commands import SlashCommandGroup
+from discord.ext import commands, pages
 
 load_dotenv() # load all the variables from the env file
+
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -22,6 +26,9 @@ def fix_name(name):
         new_name += char
             
     return new_name
+
+def calculate_score():
+    return random.randint(1, 100)
 
 @bot.event
 async def on_ready():
@@ -43,14 +50,21 @@ async def on_raw_reaction_add(payload):
                     bot.get_channel(payload.channel_id), 
                     bot.get_channel(payload.channel_id).guild.name,
                     datetime.now()))
+    
+    # print(message.attachments.pop(0))
 
     res = cur.execute("SELECT name FROM sqlite_master WHERE name LIKE '{}'".format(guild_name))
     if (res.fetchone() == None):
-        cur.execute("CREATE TABLE {}(id, user_name, message_id, message, score)".format(guild_name))
+        cur.execute("CREATE TABLE {}(id, user_name, message_id, message, attachments, score)".format(guild_name))
 
     res = cur.execute("SELECT message_id FROM {} WHERE message_id LIKE '{}'".format(guild_name, message.id))
     if (res.fetchone() == None):
-        cur.execute("INSERT INTO {} VALUES ({},'{}',{},'{}',{})".format(guild_name, user.id, user.name, message.id, message.content, random.randint(1, 100)))
+        score = calculate_score()
+        if (len(message.attachments) > 0):
+            attachment = message.attachments.pop(0)
+        else:
+            attachment = ""
+        cur.execute("INSERT INTO {} VALUES ({},'{}',{},'{}','{}',{})".format(guild_name, user.id, user.name, message.id, message.content, attachment, score))
         con.commit()
 
     res = cur.execute("SELECT score FROM {} WHERE message_id == {}".format(guild_name, message.id))
@@ -61,11 +75,62 @@ async def on_raw_reaction_add(payload):
 async def hello(ctx: discord.ApplicationContext):
     await ctx.respond("Hey!")
 
+async def get_messages(num_messages, guild_name, pages_made):
+    res = cur.execute("SELECT id, message, score, attachments FROM {} ORDER BY score DESC LIMIT -1 OFFSET {}".format(guild_name, pages_made*2))
+
+    data = res.fetchmany(num_messages)
+    fields = []
+    attachments = []
+    
+    i = 0
+    while i < len(data):
+        value = ""
+        name = await bot.fetch_user(data[i][0])
+        if name.global_name != None:
+            name = name.global_name
+
+        if data[i][1] == "" and data[i][3] != "":
+            value = data[i][3]
+        else:
+            value = data[i][1]
+        fields.append(
+            discord.EmbedField(
+                name = f"{name} - {data[i][2]} funny points!!",
+                value = f"{value}"
+            )
+        )
+        # attachments.append(data[i][3])
+        i = i + 1
+
+    return fields, attachments
+
+async def page_maker(guild_name, num_pages, num_messages):
+    pages = []
+    
+    i = 0
+    while i < num_pages:
+        fields, attachments = await get_messages(num_messages, guild_name, i)
+        embed = discord.Embed(title = "Funniest Messages", fields = fields)
+        # for pics in attachments:
+        #     if pics != "":
+        #         embed.image = pics
+        pages.append(embed)
+        i = i + 1
+
+    discord.ext.pages = pages
+    return discord.ext.pages
+
+@bot.slash_command(name="list-messages", description="List the messages that have been analyzed")
+async def list_messages(ctx: discord.ApplicationContext):
+    # """Demonstrates using the paginator with the default options."""
+    guild_name = ctx.guild
+    paginator = pages.Paginator(pages = await page_maker((fix_name(str(guild_name))), 3, 5))
+    await paginator.respond(ctx.interaction, ephemeral=False)
+
 con = sqlite3.connect("database.db")
 cur = con.cursor()
 
 bot.run((os.getenv('token'))) # run the bot with the token
 
-def print_all_messages(table_name):
-    for row in cur.execute("SELECT * FROM {}".format(table_name)):
-        print(row)
+# res = cur.execute("SELECT * FROM Lol")
+# print(res.fetchone())
